7ee4eba1e4dae152b8a4d7ab4c7570ba1b288ddf
Melanie Liang <melly.strearshy@gmail.com>
2017-07-03 19:40:03 -0600

    create admin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,14 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 //angular material ui
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule,MdChipsModule,MdTooltipModule } from '@angular/material';
+import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule, MdChipsModule, MdTooltipModule } from '@angular/material';
 // components
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { MenuAdminComponent } from './components/menu-admin/menu-admin.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 export const firebaseConfig = {
   apiKey: "AIzaSyAaVcA0wjZMDYdeXT6IRYInLbO4dYU6usE",
@@ -32,7 +33,8 @@ export const firebaseConfig = {
 export const ROUTES: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'menu', component: MenuAdminComponent }
+  { path: 'menu', component: MenuAdminComponent },
+  { path: 'admin', component: AdminComponent }
 ];
 
 @NgModule({
@@ -41,7 +43,8 @@ export const ROUTES: Routes = [
     TodoComponent,
     RegisterComponent,
     LoginComponent,
-    MenuAdminComponent
+    MenuAdminComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
