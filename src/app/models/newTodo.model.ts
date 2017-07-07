1853997a264d98cbad66311d0d17b066fe5f58c1
mbolanosc <mrybc96@gmail.com>
2017-07-06 21:05:00 -0600

    todo module not ready

--- src/app/models/newTodo.model.ts
+++ src/app/models/newTodo.model.ts
@@ -1,6 +1,19 @@
 export class NewTodo {
+  private user: string;
+  private category: string;
+  private todo: string;
+  private status: string;
 
-    constructor() {
+    constructor(pUser,pCategory,pTodo,pStatus) {
+      this.setUser(pUser);
+      this.setStatus(pStatus);
     }
+    getUser() { return this.user; }
+    getCategory() { return this.category; }
+    getTodo() { return this.todo; }
+    getStatus() { return this.status; }
 
+
+    setUser(pValue) { this.user = pValue; }
+    setStatus(pValue) { this.status = pValue; }
 }
