a1e126d0b409a97c3252a287f984fb77e07d60ce
meluk <kmelisu@hotmail.com>
2017-07-07 18:26:47 -0600

    Change days of the week

--- src/app/components/profile-user/profile-user.component.ts
+++ src/app/components/profile-user/profile-user.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-user.component.scss']
 })
 export class ProfileUserComponent implements OnInit {
-
+  day = 0;
+  days = [
+    {"name": "Lunes"},
+    {"name": "Martes"},
+    {"name": "Miércoles"},
+    {"name": "Jueves"},
+    {"name": "Viernes"},
+    {"name": "Sábado"},
+    {"name": "Domingo"},
+  ];
+  
   constructor() { }
 
   ngOnInit() {
   }
+
+  next(){
+   this.day += 1;
+   if (this.day == 7)
+   this.day = 0
+  }
+  
+
+  back(){
+    this.day -= 1;
+    if (this.day == -1)
+    this.day = 6
+ }
 
 }
