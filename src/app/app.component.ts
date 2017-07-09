af06a7d6f3b93fab12d3e033412ffc1e3c057459
Melanie Liang <melly.strearshy@gmail.com>
2017-07-08 20:43:55 -0600

    create a new json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/Rx';
+import { NewTodo } from './models/newTodo.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(private http: Http) {
+    this.loadFromJson();
+  }
+
+  loadData(todosUrl: string) {
+    this.http.get(todosUrl).map(res => res.json()).subscribe((data) => {
+
+      for (var user of data) {
+        let todos = new NewTodo(user.category, user.todo, user.state);
+        // this.todos.push(todos);
+      }
+
+    });
+  }
+
+
+  loadFromJson() {
+    this.loadData('../assets/data/todos.json');
+  }
+
 }
+
--- src/app/components/new-todo/new-todo.component.ts
+++ src/app/components/new-todo/new-todo.component.ts
@@ -30,17 +30,17 @@ export class NewTodoComponent implements OnInit {
     console.log("arreglo", this.todos[0].description);
 
     for (var index = 0; index < this.todos.length; index++) {
-      if(pvalue == this.todos[index].description){
+      if (pvalue == this.todos[index].description) {
         this.categoryForModel = this.todos[index].category;
-        console.log('holi soy igual ', pvalue , ' y ',this.todos[index].description, 'y mi category es:' , this.categoryForModel);
-      
-      this.newTodoObj = {
-        this.categoryForModel,
-        pvalue,
-        status: false
-      }
-      console.log('OBJETO',this.newTodoObj );
-      }else{
+        console.log('holi soy igual ', pvalue, ' y ', this.todos[index].description, 'y mi category es:', this.categoryForModel);
+
+        // this.newTodoObj = {
+        //   this.categoryForModel,
+        //   pvalue,
+        //   status: false
+        // }
+        console.log('OBJETO', this.newTodoObj);
+      } else {
         console.log('apesto a pedo');
       }
     }
