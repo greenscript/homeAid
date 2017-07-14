932b6f5e0648742f953e089d64c86cb81e01e0de
diegocano423 <diegocanocamacho23@gmail.com>
2017-07-14 00:54:46 -0600

    asignar tarea a usuario

--- src/app/components/new-todo/new-todo.component.ts
+++ src/app/components/new-todo/new-todo.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { NewTodo } from '../../models/newTodo.model';
 import { User } from '../../models/user.model';
+import { AuthService } from '../../services/auth.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-new-todo',
   templateUrl: './new-todo.component.html',
-  styleUrls: ['./new-todo.component.scss']
+  styleUrls: ['./new-todo.component.scss'],
+  providers: [AuthService]
 })
 export class NewTodoComponent implements OnInit {
+  public users: FirebaseListObservable<any>;
+  public selectedUser: FirebaseListObservable<any>;
+  public userId: string = '';
+  public usersdata: Array<any> = [];
+  public currentFamily: string = '';
+  public loadedUsers: boolean = false;
   categoryForModel;
   newTodoObj;
   //todosArray : NewTodo [];
@@ -19,14 +29,43 @@ export class NewTodoComponent implements OnInit {
     { category: "Acomodar", description: "Ordenar sala" }
   ]
 
-  constructor() { }
+  constructor(private as: AuthService, public auth: AngularFireAuth, public db: AngularFireDatabase) {}
 
   ngOnInit() {
+    this.auth.authState.subscribe(res => {
+      let props = this;
+      if (res && res.uid) {
+        props.currentFamily = res.uid;
+        console.log('logged in');
+        this.users = this.db.list(`/families/${props.currentFamily}/users`, {preserveSnapshot: true});
+        this.users
+        .subscribe(snapshots => {
+          snapshots.forEach(snapshot => {
+            console.log(snapshot.key)
+            if (!(snapshot.key === '0') && (props.loadedUsers === false)) {
+              props.usersdata.push(
+                ({
+                  key: snapshot.key,
+                  value: snapshot.val()
+                })
+              )
+              props.loadedUsers = true;
+            }
+          });
+          console.log(props.usersdata)
+        })
+      } else {
+        console.log('user not logged in');
+      }
+    });
   }
 
   addTodo(pvalue) {
     console.log('category', pvalue);
     console.log("arreglo", this.todos[0].description);
+
+    // this.selectedUser = this.db.list(`/families/${this.userId}/users/todos/`, {preserveSnapshot: true});
+    // this.selectedUser.push(this.newTodoObj);
 
     for (var index = 0; index < this.todos.length; index++) {
       if (pvalue == this.todos[index].description) {
@@ -35,7 +74,6 @@ export class NewTodoComponent implements OnInit {
 
         this.newTodoObj = new NewTodo(this.categoryForModel, pvalue, false);
 
-        this.todos.push(this.newTodoObj);
 
         console.log(this.todos);
 
@@ -44,5 +82,14 @@ export class NewTodoComponent implements OnInit {
         console.log('apesto a pedo');
       }
     }
+  }
+
+  selectUser(pUid) {
+    console.log(pUid);
+    this.userId = pUid;
+    // esto de abajo es como agregar el todo a la base de datos
+    // deberia de ir en la funcion addTodo
+    this.selectedUser = this.db.list(`/families/${this.currentFamily}/users/${this.userId}/todos/`, {preserveSnapshot: true});
+    this.selectedUser.push({name: 'asd', description: 'asd', category: 'asd'});
   }
 }
