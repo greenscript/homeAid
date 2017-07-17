15b25bea383bf0131c8db41f9111c99f1b4e4a06
Melanie Liang <m.liang@paralleldevs.com>
2017-07-16 19:25:09 -0600

    create limpiar modal, cocinar modal, mascotas modal and personal modal

--- src/app/components/new-todo/new-todo.component.ts
+++ src/app/components/new-todo/new-todo.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { NewTodo } from '../../models/newTodo.model';
 import { User } from '../../models/user.model';
-import { AuthService } from '../../services/auth.service';
-import { AngularFireAuth } from 'angularfire2/auth';
+import {  AuthService } from '../../services/auth.service';
+import {  AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
@@ -23,41 +23,41 @@ export class NewTodoComponent implements OnInit {
   //todosArray : NewTodo [];
 
   todos = [
-    { category: "Acomodar", description: "Alistar el comedor.",status: false, relevance: 'none'},
-    { category: "Acomodar", description: "Doblar ropa.",status: false, relevance: 'none'},
-    { category: "Acomodar", description: "Planchar ropa.",status: false, relevance: 'none'},
-    { category: "Acomodar", description: "Ordenar baños.",status: false, relevance: 'none'},
-    { category: "Acomodar", description: "Ordenar cocinar.",status: false, relevance: 'none'},
-    { category: "Acomodar", description: "Ordenar sala.",status: false, relevance: 'none'},
+    { category: "Acomodar", description: "Alistar el comedor.", status: false, relevance: 'none' },
+    { category: "Acomodar", description: "Doblar ropa.", status: false, relevance: 'none' },
+    { category: "Acomodar", description: "Planchar ropa.", status: false, relevance: 'none' },
+    { category: "Acomodar", description: "Ordenar baños.", status: false, relevance: 'none' },
+    { category: "Acomodar", description: "Ordenar cocinar.", status: false, relevance: 'none' },
+    { category: "Acomodar", description: "Ordenar sala.", status: false, relevance: 'none' },
     //Cat: limpiar.
-    { category: "Limpiar", description: "Barrer.",status: false, relevance: 'none'},
-    { category: "Limpiar", description: "Pasar el palo piso.",status: false, relevance: 'none'},
-    { category: "Limpiar", description: "Lavar platos.",status: false, relevance: 'none'},
-    { category: "Limpiar", description: "Aspirar la alfombra y muebles.",status: false, relevance: 'none'},
-    { category: "Limpiar", description: "Limpiar el inodoro y la ducha.",status: false, relevance: 'none'},
-    { category: "Limpiar", description: "Sacar la basura.",status: false, relevance: 'none'},
+    { category: "Limpiar", description: "Barrer.", status: false, relevance: 'none' },
+    { category: "Limpiar", description: "Pasar el palo piso.", status: false, relevance: 'none' },
+    { category: "Limpiar", description: "Lavar platos.", status: false, relevance: 'none' },
+    { category: "Limpiar", description: "Aspirar la alfombra y muebles.", status: false, relevance: 'none' },
+    { category: "Limpiar", description: "Limpiar el inodoro y la ducha.", status: false, relevance: 'none' },
+    { category: "Limpiar", description: "Sacar la basura.", status: false, relevance: 'none' },
     //Cat: Cocinar.
-    { category: "Cocinar", description: "Desayuno.",status: false, relevance: 'none'},
-    { category: "Cocinar", description: "Almuerzo.",status: false, relevance: 'none'},
-    { category: "Cocinar", description: "Merienda.",status: false, relevance: 'none'},
-    { category: "Cocinar", description: "Cena.",status: false, relevance: 'none'},
+    { category: "Cocinar", description: "Desayuno.", status: false, relevance: 'none' },
+    { category: "Cocinar", description: "Almuerzo.", status: false, relevance: 'none' },
+    { category: "Cocinar", description: "Merienda.", status: false, relevance: 'none' },
+    { category: "Cocinar", description: "Cena.", status: false, relevance: 'none' },
     //Cat: Mascotas.
-    { category: "Mascotas", description: "Alimentar la mascota.",status: false, relevance: 'none'},
-    { category: "Mascotas", description: "Bañar la mascota.",status: false, relevance: 'none'},
-    { category: "Mascotas", description: "Pasear a la mascota.",status: false, relevance: 'none'},
-    { category: "Mascotas", description: "Limpiar la casa de la mascota.",status: false, relevance: 'none'},
-    { category: "Mascotas", description: "Limpiar las necesidades de la mascota.",status: false, relevance: 'none'},
+    { category: "Mascotas", description: "Alimentar la mascota.", status: false, relevance: 'none' },
+    { category: "Mascotas", description: "Bañar la mascota.", status: false, relevance: 'none' },
+    { category: "Mascotas", description: "Pasear a la mascota.", status: false, relevance: 'none' },
+    { category: "Mascotas", description: "Limpiar la casa de la mascota.", status: false, relevance: 'none' },
+    { category: "Mascotas", description: "Limpiar el popo de la mascota.", status: false, relevance: 'none' },
     //Cat: Personal.
-    { category: "Personal", description: "Limpiar el cuarto.",status: false, relevance: 'none'},
-    { category: "Personal", description: "Acomodar el cuarto.",status: false, relevance: 'none'},
-    { category: "Personal", description: "Lavar y secar ropa sucia.",status: false, relevance: 'none'},
-    { category: "Personal", description: "Tender la cama.",status: false, relevance: 'none'},
-    { category: "Personal", description: "Hacer tareas academicas.",status: false, relevance: 'none'},
-    { category: "Personal", description: "Planchar y doblar ropa.",status: false, relevance: 'none'},
+    { category: "Personal", description: "Limpiar el cuarto.", status: false, relevance: 'none' },
+    { category: "Personal", description: "Acomodar el cuarto.", status: false, relevance: 'none' },
+    { category: "Personal", description: "Lavar y secar ropa sucia.", status: false, relevance: 'none' },
+    { category: "Personal", description: "Tender la cama.", status: false, relevance: 'none' },
+    { category: "Personal", description: "Hacer tareas academicas.", status: false, relevance: 'none' },
+    { category: "Personal", description: "Planchar y doblar ropa.", status: false, relevance: 'none' },
 
   ]
 
-  constructor(private as: AuthService, public auth: AngularFireAuth, public db: AngularFireDatabase) {}
+  constructor(private as: AuthService, public auth: AngularFireAuth, public db: AngularFireDatabase) { }
 
   ngOnInit() {
     this.auth.authState.subscribe(res => {
@@ -65,23 +65,23 @@ export class NewTodoComponent implements OnInit {
       if (res && res.uid) {
         props.currentFamily = res.uid;
         console.log('logged in');
-        this.users = this.db.list(`/families/${props.currentFamily}/users`, {preserveSnapshot: true});
+        this.users = this.db.list(`/families/${props.currentFamily}/users`, { preserveSnapshot: true });
         this.users
-        .subscribe(snapshots => {
-          snapshots.forEach(snapshot => {
-            console.log(snapshot.key)
-            if (!(snapshot.key === '0') && (props.loadedUsers === false)) {
-              props.usersdata.push(
-                ({
-                  key: snapshot.key,
-                  value: snapshot.val()
-                })
-              )
-            }
-          });
-          console.log(props.usersdata)
-          props.loadedUsers = true;
-        })
+          .subscribe(snapshots => {
+            snapshots.forEach(snapshot => {
+              console.log(snapshot.key)
+              if (!(snapshot.key === '0') && (props.loadedUsers === false)) {
+                props.usersdata.push(
+                  ({
+                    key: snapshot.key,
+                    value: snapshot.val()
+                  })
+                )
+              }
+            });
+            console.log(props.usersdata)
+            props.loadedUsers = true;
+          })
       } else {
         console.log('user not logged in');
       }
@@ -98,7 +98,7 @@ export class NewTodoComponent implements OnInit {
         console.log('holi soy igual ', pvalue, ' y ', this.todos[index].description, 'y mi category es:', this.categoryForModel);
 
         //this.newTodoObj = new NewTodo(this.categoryForModel, pvalue, false);
-      //  console.log(this.todos);
+        //  console.log(this.todos);
 
       } else {
         // xD
@@ -113,8 +113,8 @@ export class NewTodoComponent implements OnInit {
     this.userId = pUid;
     // esto de abajo es como agregar el todo a la base de datos
     // deberia de ir en la funcion addTodo
-    this.selectedUser = this.db.list(`/families/${this.currentFamily}/users/${this.userId}/todos/`, {preserveSnapshot: true});
+    this.selectedUser = this.db.list(`/families/${this.currentFamily}/users/${this.userId}/todos/`, { preserveSnapshot: true });
     // en vez de este objeto, iria el new Todo(parametros)
-    this.selectedUser.push({name: 'asd', description: 'asd', category: 'asd'});
+    this.selectedUser.push({ name: 'asd', description: 'asd', category: 'asd' });
   }
 }
