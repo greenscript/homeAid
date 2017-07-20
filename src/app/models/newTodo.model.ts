cc95734fd3a8177e5dc79683ffb8c7254318c357
mbolanosc <mrybc96@gmail.com>
2017-07-19 19:07:04 -0600

    edit todo model

--- src/app/models/newTodo.model.ts
+++ src/app/models/newTodo.model.ts
@@ -5,33 +5,33 @@ export class NewTodo {
   private category: string;
   private todo: string;
   private status: boolean = false;
-  // private day : number;
+  private day : number;
   //  private points: string;
   //todos temporal
   private todos: Array<any>;
 
 
 
-  constructor(pCategory, pTodo, pStatus) {
+  constructor(pCategory, pTodo, pStatus,pDay) {
     //  this.setUser(pUser);
     this.setCategory(pCategory);
     this.setTodo(pTodo);
     this.setStatus(pStatus);
-    // this.setDay(pDay);
+    this.setDay(pDay);
     //  this.setPoints(pPoints);
   }
   //  getUser() { return this.user; }
   getCategory() { return this.category; }
   getTodo() { return this.todo; }
   getStatus() { return this.status; }
-  // getDay() {return this.day;}
+  getDay() {return this.day;}
   //  getPoints() { return this.points; }
 
   //  setUser(pValue) { this.user = pValue; }
   setCategory(pValue) { this.category = pValue; }
   setTodo(pValue) { this.todo = pValue; }
   setStatus(pValue) { this.status = pValue; }
-  // setDay(pValue) { this.day = pValue;}
+  setDay(pValue) { this.day = pValue;}
   //  setPoints(pValue) { this.points = pValue; }
   //  push para tareas?
   addTodoToTodos(Objtodo) { this.todos.push(Objtodo) }
