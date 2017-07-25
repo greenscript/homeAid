7f18526d91afb51eab97bf5fa2e5feba0575a09b
greenscript <diegocanocamacho23@gmail.com>
2017-07-24 20:37:39 -0600

    mobile bar improvments

--- src/app/components/mobile-bar/mobile-bar.component.ts
+++ src/app/components/mobile-bar/mobile-bar.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 @Component({
   selector: 'app-mobile-bar',
   templateUrl: './mobile-bar.component.html',
   styleUrls: ['./mobile-bar.component.scss']
 })
 export class MobileBarComponent implements OnInit {
+  public dashboard: boolean;
+  public users: boolean;
+  public menu: boolean;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    switch (this.router.url) {
+      case '/dashboard':
+        this.dashboard = true;
+      break;
+      case '/users':
+        this.users = true;
+      break;
+      case '/menu':
+        this.menu = true;
+      break;
+    }
   }
 
 }
