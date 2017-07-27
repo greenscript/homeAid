76ac3d4e2f9cadfa91ab0dfcd189ff4ad492542a
mbolanosc <mrybc96@gmail.com>
2017-07-26 23:25:47 -0600

    start process of complete a todo

--- src/app/components/detail-todo/detail-todo.component.ts
+++ src/app/components/detail-todo/detail-todo.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { AuthService } from '../../services/auth.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-detail-todo',
   templateUrl: './detail-todo.component.html',
-  styleUrls: ['./detail-todo.component.scss']
+  styleUrls: ['./detail-todo.component.scss'],
+  providers: [DataService, AuthService]
+
 })
 export class DetailTodoComponent implements OnInit {
+  todoId;
+  currentTodo;
+  uid:string;
 
-  constructor() { }
+  constructor(private as: AuthService, public auth: AngularFireAuth, public db: AngularFireDatabase, private http: Http, private route: ActivatedRoute, public ds: DataService) {
+    this.todoId = route.snapshot.paramMap.get('id');
+    console.log('id del todo',this.todoId);
+  }
 
   ngOnInit() {
   }
-
 }
