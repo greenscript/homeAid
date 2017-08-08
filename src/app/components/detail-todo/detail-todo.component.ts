a3fd207db2744173d282fa45bc3dd4cda0a257fb
Melanie Liang <m.liang@paralleldevs.com>
2017-08-08 13:19:10 -0600

    add modal button function, not ready

--- src/app/components/detail-todo/detail-todo.component.ts
+++ src/app/components/detail-todo/detail-todo.component.ts
@@ -136,4 +136,23 @@ export class DetailTodoComponent implements OnInit {
       })
     })
   }
+  selectUser(pUid) {
+    console.log("select: " + pUid);
+    this.userId = pUid;
+  }
+
+  send() {
+    for (var i = 0; i < this.usersArr.length; i++) {
+      if (this.userId) {
+        this.usersArr.push(this.currentTodo)
+        console.log("user id: ", this.userId);
+        console.log("user array: ", this.usersArr);
+        console.log("user todo: ", this.currentTodo);
+
+      } else {
+        console.log("select a user first please");
+
+      }
+    }
+  }
 }
